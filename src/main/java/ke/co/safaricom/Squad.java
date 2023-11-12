7b5b6c836e3d2ad55d903b800dd85526a8ae0f30
deusnyakundi <nyakundideus1@gmail.com>
2023-11-12 16:49:22 +0300

    Impleting Squad Class

--- src/main/java/ke/co/safaricom/Squad.java
+++ src/main/java/ke/co/safaricom/Squad.java
@@ -1,4 +1,23 @@
 package ke.co.safaricom;
 
+import java.util.ArrayList;
+
 public class Squad {
+    private int squadId;
+    private String squadName;
+    private int squadSize;
+    private String cause;
+    private ArrayList<Hero>  squadMembers = new ArrayList<>();
+    private static ArrayList<Squad> instances = new ArrayList<>();
+
+
+    public Squad(String name, int size, String cause ){
+        squadName = name;
+        squadSize = size;
+        this.cause = cause;
+        this.squadMembers = new ArrayList<>();
+        instances.add(this);
+        this.squadId = instances.size();
+
+    }
 }
