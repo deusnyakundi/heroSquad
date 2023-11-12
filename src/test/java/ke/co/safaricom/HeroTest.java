f4e7bc0e61f1c24756129fa84e1c74c83ab77ba1
deusnyakundi <nyakundideus1@gmail.com>
2023-11-12 16:46:33 +0300

    Performing tests for Hero Class

--- src/test/java/ke/co/safaricom/HeroTest.java
+++ src/test/java/ke/co/safaricom/HeroTest.java
@@ -6,4 +6,30 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HeroTest {
 
+    @Test
+    public void newHero_instantiatesCorrectly_true() {
+        Hero newHero = Hero.setUpNewHero();
+        assertTrue(newHero instanceof Hero);
+    }
+    @Test
+    public void newHero_getName_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("Arrow",newHero.getName());
+    }
+    @Test
+    public void newHero_getAge_Int() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals(23,newHero.getAge());
+    }
+    @Test
+    public void newHero_getPower_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("Master Archer",newHero.getPower());
+    }
+    @Test
+    public void newHero_getWeakness_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("Human",newHero.getWeakness());
+    }
+
 }
