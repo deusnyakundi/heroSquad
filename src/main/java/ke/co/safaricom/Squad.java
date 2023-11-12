6522ea31ed76f9d1e4f94d8533466aba3d9f4de6
deusnyakundi <nyakundideus1@gmail.com>
2023-11-12 16:51:28 +0300

    Implementing Squad Class defaults and methods

--- src/main/java/ke/co/safaricom/Squad.java
+++ src/main/java/ke/co/safaricom/Squad.java
@@ -19,4 +19,25 @@ public class Squad {
         instances.add(this);
         this.squadId = instances.size();
     }
+
+    public int getSquadId(){return squadId;}
+    public static Squad findBySquadId(int id) {return instances.get(id-1);}
+    public String getSquadName() {return squadName;}
+    public int getSize() {return squadSize;}
+    public String getCause() {return this.cause;}
+    public static ArrayList<Squad> getInstances(){return instances;}
+    public ArrayList<Hero> getSquadMembers(){
+        return squadMembers;
+    }
+    public void setSquadMembers(Hero newMember) {
+        squadMembers.add(newMember);
+    }
+    public static void clearAllSquads(){ instances.clear(); }
+    public void clearAllSquadMembers(){ getSquadMembers().clear(); }
+
+    public static Squad setUpNewSquad(){return new Squad("Avengers",15,"Infinity Stone");}
+    public static Squad setUpNewSquad1(){return new Squad("Peaky Blinder",15,"Birmingham Victory");}
+
+}
+
 }
