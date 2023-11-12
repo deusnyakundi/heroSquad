5993df46345216aca4f4f1960f4a7b9231d38c04
deusnyakundi <nyakundideus1@gmail.com>
2023-11-12 16:43:06 +0300

    Impleting Hero Class

--- src/main/java/ke/co/safaricom/Hero.java
+++ src/main/java/ke/co/safaricom/Hero.java
@@ -1,4 +1,21 @@
 package ke.co.safaricom;
 
+import java.util.ArrayList;
+
 public class Hero {
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private int id;
+    private static ArrayList<Hero> instances = new ArrayList<>();
+
+    public Hero(String name, Integer age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+        instances.add(this);
+        this.id = instances.size();
+    }
 }
